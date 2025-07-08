42cea45bfcb561d104fe392ef2de8e0dcc780532
MDNoDX <mdnodx@gmail.com>
2025-07-08 12:15:01 +0500

    lessons va themes jadvallari ochildi ; ;

--- modules.py
+++ modules.py
@@ -1,4 +1,16 @@
-from sqlalchemy.orm import DeclarativeBase
-
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import String
 class Base(DeclarativeBase):
     pass
+
+class Lesson(Base):
+    __tablename__ = "lessons"
+
+    id: Mapped[int] = mapped_column(primary_key = True)
+    name: Mapped[str] = mapped_column(String(50))
+
+class Themes(Base):
+    __tablename__ = "themes"
+
+    id: Mapped[int] = mapped_column(primary_key = True)
+    title: Mapped[str] = mapped_column(String(50))
